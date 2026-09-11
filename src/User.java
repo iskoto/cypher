e9e05657e28750ecb03642bc12d8e923671e96cd
Ismail Yildirim <145703561+iskoto@users.noreply.github.com>
2026-09-11 22:52:15 +0300

    Create User.java

---
+++ src/User.java
@@ -0,0 +1,8 @@
+//  SID 2118755 ISMAIL YILDIRIM TEAM CYPHER
+public class User {
+    public String name;
+    public String email;
+    public String sortcode;
+    public String accountNumber;
+    public String password;
+}

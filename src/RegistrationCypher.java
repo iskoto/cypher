3994d1473a577f1b31eefe260b67fe57870b1b8a
Ismail Yildirim <145703561+iskoto@users.noreply.github.com>
2026-09-11 22:50:16 +0300

    Create RegistrationCypher.java

---
+++ src/RegistrationCypher.java
@@ -0,0 +1,135 @@
+//  SID 2118755 ISMAIL YILDIRIM TEAM CYPHER
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+
+public class RegistrationCypher extends JDialog {
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfSortCode;
+    private JTextField tfAccountNumber;
+    private JPasswordField pfPassword;
+    private JPasswordField pfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JPanel registerPanel;
+
+    public RegistrationCypher(JFrame parent)  {
+        super(parent);
+        setTitle("Create a new account");
+        setContentPane(registerPanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerUser();
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void registerUser() {
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String sortcode = tfSortCode.getText();
+        String accountNumber = tfAccountNumber.getText();
+        String password = String.valueOf(pfPassword.getPassword());
+        String confirmPassword = String.valueOf(pfConfirmPassword.getPassword());
+
+        if (name.isEmpty() || email.isEmpty() || sortcode.isEmpty() || accountNumber.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            JOptionPane.showMessageDialog(this,
+                    "Please enter all fields",
+                    "Try Again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            JOptionPane.showMessageDialog(this,
+                    "Confirm Password does not match",
+                    "Try Again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        user = addUserToDatabase(name, email, sortcode, accountNumber, password);
+        if (user != null) {
+            dispose();
+        }
+        else {
+            JOptionPane.showMessageDialog(this,
+                    "Failed to register New Cypher User",
+                    "Please try again",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public User user;
+    private User addUserToDatabase(String name, String email, String sortcode, String accountNumber, String password) {
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/MyStore?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, sortcode, accountNumber, password)" +
+                    "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,email);
+            preparedStatement.setString(3,sortcode);
+            preparedStatement.setString(4,accountNumber);
+            preparedStatement.setString(5,password);
+
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0){
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.sortcode = sortcode;
+                user.accountNumber = accountNumber;
+                user.password = password;
+            }
+
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public static void main(String[] args) {
+        RegistrationCypher myForm = new RegistrationCypher(null);
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Successful registration of: " + user.name);
+        }
+        else {
+            System.out.println("Registration cancelled");
+        }
+    }
+}
